9096b6f5d3403c1d51cc4e45569ab5d89426d90d
Omar Pardo <omarpardog@gmail.com>
2018-11-30 18:27:47 -0500

    Delete unnecessary packages in the read_data file.

--- ConferenceRoom/read_data.R
+++ ConferenceRoom/read_data.R
@@ -1,6 +1,4 @@
 library(gsheet)
-library(dplyr)
-library(ggplot2)
 
 theoffice_url <- 'docs.google.com/spreadsheets/d/18wS5AAwOh8QO95RwHLS95POmSNKA2jjzdt0phrxeAE0'
 theoffice_df <- gsheet2tbl(theoffice_url)
