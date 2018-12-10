c482be70c6b0f5321be9d30e356f7015fd654e85
Antonio Lopez <antonio1695@gmail.com>
2018-12-10 17:31:11 -0500

    Adapt graphs, homogenizing shiny + rmd

--- ConferenceRoom/functions/functions.R
+++ ConferenceRoom/functions/functions.R
@@ -520,3 +520,74 @@ viewers_graph <-  function(){
     xlim(c(0,205)) +
     ylim(c(0,25)) 
 }
+
+get_most_shared_scenes <- function(words_df, char, seasons) {
+  char_scenes_df <- get_char_scenes(words_df, char, seasons)
+  if(nrow(char_scenes_df) > 0) {
+    shared_scenes_df <- get_shared_scenes(words_df, char, char_scenes_df)
+    most_shared_scenes_plot <- get_most_shared_scenes_plot(shared_scenes_df)
+    return(most_shared_scenes_plot)
+  } else {
+    return()
+  }
+}
+
+get_char_scenes <- function(words_df, char, seasons){
+  char_scenes_df <- words_df %>% 
+    filter_words_df(char, seasons) %>% 
+    select(season, episode, scene) %>% 
+    unique() %>% 
+    mutate(scene_id = 1:nrow(.))
+  return(char_scenes_df)
+}
+
+get_shared_scenes <- function(words_df, char, char_scenes_df){
+  shared_scenes_df <- words_df %>% 
+    filter(speaker != char) %>% 
+    inner_join(char_scenes_df, by = c("season", "episode", "scene")) %>% 
+    group_by(speaker) %>% 
+    summarise(shared_scenes = length(unique(scene_id))) %>% 
+    top_n(10, shared_scenes) %>% 
+    mutate(speaker = fct_reorder(factor(str_to_title(speaker)), -shared_scenes))
+  return(shared_scenes_df)
+}
+
+get_most_shared_scenes_plot <- function(shared_scenes_df) {
+  shared_scenes_df$speaker <- tolower(shared_scenes_df$speaker)
+  most_shared_scenes_plot <- ggplot(
+    data = shared_scenes_df,
+    aes(x = reorder(speaker, -shared_scenes), y = shared_scenes, fill= speaker)
+  ) +
+    geom_bar(stat = "identity") +
+    xlab("") +
+    ylab("") +
+    scale_colour_manual(values = MyPalette, aesthetics = "fill") 
+  return(most_shared_scenes_plot)
+}
+
+phrase_graph <- function(words_df){
+  thats_what_she_said <- words_df %>% 
+    filter(word == "thats_what_she_said") %>% 
+    group_by(speaker) %>% 
+    summarise(Freq = n()) %>% 
+    arrange(-Freq)
+  
+  ggplot(thats_what_she_said, aes(x=reorder(speaker, Freq), y=Freq, fill = speaker)) +
+    geom_col(color = 'black') + 
+    coord_flip() +
+    labs(title = "    'That'\n                  -She", 
+         x = "", 
+         y="Total number of gags") +
+    scale_colour_manual(values = MyPalette, aesthetics = "fill") +
+    theme(legend.position="none")
+}
+
+filter_words_df <- function(words_df, character, seasons){
+  if(!is.null(seasons)) {
+    filtered_words_data <- words_df %>% 
+      filter(speaker == character, season %in% seasons)
+    return(filtered_words_data)
+  } else {
+    return()
+  }
+}
